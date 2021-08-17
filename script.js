eea5f1065e79f13d800927c857cedf4112e9124a
matthew6 <m.holland793@gmail.com>
2021-08-17 17:03:20 -0400

    got form submissions working

--- script.js
+++ script.js
@@ -13,4 +13,14 @@ const itemAmount = document.getElementById("itemCost");
 
 itemForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  
+  // Get the new item cost 
+  const newItemCost = parseInt(itemAmount.value);
+  console.log(newItemCost);
+
+  // Get the category of the new item
+  const purchaseCategoryElement = document.getElementById("purchases");
+  const purchaseCategory = purchaseCategoryElement.value;
+  console.log(purchaseCategory)
+
 });
