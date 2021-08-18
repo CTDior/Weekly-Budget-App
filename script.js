827f41bbc85c717fd8bd8daa7fa69718c87cd51e
matthew6 <m.holland793@gmail.com>
2021-08-18 14:10:16 -0400

    new item cost is working. Need to get remaining balance to work too

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
-const budgetForm = document.getElementById("startingBudget");
-const startingBudget = document.getElementById("weeklyBudgetAmount");
-const initialBudget = document.getElementById("weeklyBudgetTotal");
-const newPurchase = document.getElementById("purchases");
-let entTotal = parseFloat(document.getElementById("entertainment").innerHTML);
-const foodTotal = document.getElementById("food");
-const clothingTotal = document.getElementById("clothing");
-const billsTotal = document.getElementById("bills");
+let budgetForm = document.getElementById("startingBudget");
+let startingBudget = document.getElementById("weeklyBudgetAmount");
+let initialBudget = document.getElementById("weeklyBudgetTotal");
+let newPurchase = document.getElementById("purchases");
+let entTotal = 0;
+let entTotalDisplay = document.getElementById("entertainment");
+let foodTotal = 0;
+let foodTotalDisplay = document.getElementById("food");
+let clothingTotal = 0;
+let clothingTotalDisplay = document.getElementById("clothing");
+let billsTotal = 0;
+let billsTotalDisplay = document.getElementById("bills");
+var budgetAmount;
+let remainingBalance;
+// let remainingBalance = budgetAmount;
+// console.log(budgetAmount);
+
 
 budgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const budgetAmount = parseInt(startingBudget.value);
-  console.log(budgetAmount);
+  let budgetAmount = parseInt(startingBudget.value);
   initialBudget.innerHTML = budgetAmount;
 });
 
-const itemForm = document.getElementById("itemForm");
-const purchases = document.getElementById("purchases");
-const itemAmount = document.getElementById("itemCost");
+let itemForm = document.getElementById("itemForm");
+let purchases = document.getElementById("purchases");
+let itemAmount = document.getElementById("itemCost");
 
 itemForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get the new item cost
-  const newItemCost = parseFloat(itemAmount.value);
-  console.log(typeof newItemCost);
+  let newItemCost = parseFloat(itemAmount.value);
+  
   // Get the category of the new item
-  const purchaseCategoryElement = document.getElementById("purchases");
-  const purchaseCategory = purchaseCategoryElement.value;
-  console.log(purchaseCategory);
+  let purchaseCategoryElement = document.getElementById("purchases");
+  let purchaseCategory = purchaseCategoryElement.value;
+  
   // Match the category to the total
   if (purchaseCategory === "entertainment") {
-    console.log(entTotal);
     // the following line is not adding the newItemCost to entTotal
     entTotal += newItemCost;
-    entTotal.innerText = entTotal;
-    console.log(entTotal);
-    console.log(newItemCost);
-    console.log(newPurchase);
+    entTotalDisplay.innerText = entTotal;    
   } else if (purchaseCategory === "food") {
-    foodTotal.innerHTML += newItemCost;
+    foodTotal += newItemCost;
+    foodTotalDisplay.innerText = foodTotal;
   } else if (purchaseCategory === "clothing") {
-    clothingTotal.innerHTML += newItemCost;
+    clothingTotal += newItemCost;
+    clothingTotalDisplay.innerText = clothingTotal;
   } else if (purchaseCategory === "bills") {
-    billsTotal.innerHTML += newItemCost;
+    billsTotal += newItemCost;
+    billsTotalDisplay.innerText = billsTotal;
   }
+
+ 
+
 });
+
+ // Subtract newItemCost from weeklyBudgetTotal
+  // and display the remaining balance. 
+  // budget amount - newItemCost = remainingBalance
+  // let updatedBudgetAmount = parseFloat(startingBudget.value);
+  // updatedBudgetAmount - newItemCost;
+  // console.log(remainingBalance);
+
+
+  // Current problem: We cannot get a handle on the initial 
+  // starting budget number to use as a starting value. We 
+  // want to use this number, then subtract each new item 
+  // from it to display the remaining balance. Or, use a loop
+  // to go over the totals from the four categories and 
+  // subtract them from the starting budget value.  
