a2f7fec745359a9e5f3d02682605256871a165b3
CTDior <83152946+CTDior@users.noreply.github.com>
2021-08-18 11:22:13 -0400

    increased functionality

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const budgetForm = document.getElementById("startingBudget");
 const startingBudget = document.getElementById("weeklyBudgetAmount");
+const initialBudget = document.getElementById("weeklyBudgetTotal");
+const newPurchase = document.getElementById("purchases");
+const entTotal = document.getElementById("entertainment");
+const foodTotal = document.getElementById("food");
+const clothingTotal = document.getElementById("clothing");
+const billsTotal = document.getElementById("bills");
+
 budgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const budgetAmount = parseInt(startingBudget.value);
   console.log(budgetAmount);
+  initialBudget.innerHTML = budgetAmount;
 });
 
 const itemForm = document.getElementById("itemForm");
@@ -13,14 +20,22 @@ const itemAmount = document.getElementById("itemCost");
 
 itemForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  // Get the new item cost 
-  const newItemCost = parseInt(itemAmount.value);
-  console.log(newItemCost);
 
+  // Get the new item cost
+  const newItemCost = parseInt(itemAmount.value);
+  console.log(typeof entTotal);
   // Get the category of the new item
   const purchaseCategoryElement = document.getElementById("purchases");
   const purchaseCategory = purchaseCategoryElement.value;
-  console.log(purchaseCategory)
-
+  console.log(purchaseCategory);
+  // Match the category to the total
+  if (purchaseCategory === "entertainment") {
+    entTotal.innerHTML += newItemCost;
+  } else if (purchaseCategory === "food") {
+    foodTotal.innerHTML += newItemCost;
+  } else if (purchaseCategory === "clothing") {
+    clothingTotal.innerHTML += newItemCost;
+  } else if (purchaseCategory === "bills") {
+    billsTotal.innerHTML += newItemCost;
+  }
 });
