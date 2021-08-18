411ec707b492e18267456936d13e80e1a9e5f50c
matthew6 <m.holland793@gmail.com>
2021-08-18 12:02:39 -0400

    if block not adding newItemCost to totals

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const budgetForm = document.getElementById("startingBudget");
 const startingBudget = document.getElementById("weeklyBudgetAmount");
 const initialBudget = document.getElementById("weeklyBudgetTotal");
 const newPurchase = document.getElementById("purchases");
-const entTotal = document.getElementById("entertainment");
+const entTotal = parseFloat(document.getElementById("entertainment").innerHTML);
 const foodTotal = document.getElementById("food");
 const clothingTotal = document.getElementById("clothing");
 const billsTotal = document.getElementById("bills");
@@ -22,15 +22,20 @@ itemForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get the new item cost
-  const newItemCost = parseInt(itemAmount.value);
-  console.log(typeof entTotal);
+  const newItemCost = parseFloat(itemAmount.value);
+  console.log(typeof newItemCost);
   // Get the category of the new item
   const purchaseCategoryElement = document.getElementById("purchases");
   const purchaseCategory = purchaseCategoryElement.value;
   console.log(purchaseCategory);
   // Match the category to the total
   if (purchaseCategory === "entertainment") {
+    console.log(entTotal);
+    // the following line is not adding the newItemCost to entTotal
     entTotal.innerHTML += newItemCost;
+    console.log(entTotal);
+    console.log(newItemCost);
+    console.log(newPurchase);
   } else if (purchaseCategory === "food") {
     foodTotal.innerHTML += newItemCost;
   } else if (purchaseCategory === "clothing") {
