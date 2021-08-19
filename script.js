3f8c85d17ddc6b7f122178e9742e3cad8932f725
matthew6 <m.holland793@gmail.com>
2021-08-19 14:01:06 -0400

    functionality just about done

--- script.js
+++ script.js
@@ -9,7 +9,16 @@ let billsTotalDisplay = document.getElementById("bills");
 let totalExpensesDisplay = document.getElementById("totalExpenses");
 let remainingBalanceDisplay = document.getElementById("remainingBalance");
 let miscTotalDisplay = document.getElementById("misc");
+// The new ul lists
 let entItemList = document.getElementById("entItemList");
+let foodItemList = document.getElementById("foodItemList");
+let clothingItemList = document.getElementById("clothingItemList");
+let billsItemList = document.getElementById("billsItemList");
+let miscItemList = document.getElementById("miscItemList");
+
+
+let newItemName = document.getElementById("itemName");
+let newItemNameValue = newItemName.value;
 
 let entTotal = 0;
 let foodTotal = 0;
@@ -19,7 +28,7 @@ let totalExpenses = 0;
 let remainingBalance = 0;
 let budgetTotal = 0;
 let miscTotal = 0;
-let foodItemList;
+
 
 budgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -47,27 +56,43 @@ itemForm.addEventListener("submit", (e) => {
   let purchaseCategoryElement = document.getElementById("purchases");
   let purchaseCategory = purchaseCategoryElement.value;
 
+  // Make a new item 
+  let newItem = document.createElement("li");
+  // Put the entered name (from the form) into the new item
+
+  
+  if (remainingBalance < newItemCost) {
+    alert("Purchasing this item will put you over budget!");
+  }
+  
+
+
   // Match the category to the total
   if (purchaseCategory === "entertainment") {
-    // the following line is not adding the newItemCost to entTotal
     entTotal += newItemCost;
     entTotalDisplay.innerText = entTotal;
-    let newItem = document.createElement("li");
-    newItem.add(purchases);
-    newItem.innerText = purchases;
-    entItemList.append(newItem);
-  } else if (purchaseCategory === "food") {
+    newItem.innerText = `${newItemName.value}: $${newItemCost}`;
+    entItemList.appendChild(newItem);
+    } else if (purchaseCategory === "food") {
     foodTotal += newItemCost;
     foodTotalDisplay.innerText = foodTotal;
+    newItem.innerText = `${newItemName.value}: $${newItemCost}`;
+    foodItemList.appendChild(newItem);
   } else if (purchaseCategory === "clothing") {
     clothingTotal += newItemCost;
     clothingTotalDisplay.innerText = clothingTotal;
+    newItem.innerText = `${newItemName.value}: $${newItemCost}`;
+    clothingItemList.appendChild(newItem);
   } else if (purchaseCategory === "bills") {
     billsTotal += newItemCost;
     billsTotalDisplay.innerText = billsTotal;
+    newItem.innerText = `${newItemName.value}: $${newItemCost}`;
+    billsItemList.appendChild(newItem);
   } else if (purchaseCategory === "misc") {
     miscTotal += newItemCost;
     miscTotalDisplay.innerText = miscTotal;
+    newItem.innerText = `${newItemName.value}: $${newItemCost}`;
+    miscItemList.appendChild(newItem);
   }
 
   totalExpenses = clothingTotal + foodTotal + entTotal + billsTotal + miscTotal;
